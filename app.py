bed4bfdbd2370a000285fa2c211e7dae6f02bba0
ChenNuode <chennuode2012@gmail.com>
2019-09-13 22:59:52 +0800

    Added frontend for inventory

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import *
+
+app = Flask(__name__)
+app.secret_key = "fixed"
+import sqlite3 as sql
+
+@app.route('/')
+def hello():
+    return render_template("dashboard.html")
+
+@app.route('/inventory')
+def inventory():
+    #return "Hello World!"
+    return render_template("inventory.html")
+
+"""
+@app.route('/staff')
+def staff():
+    #return "Hello World!"
+    return render_template("dashboard.html")
+
+@app.route('/requests')
+def requests():
+    #return "Hello World!"
+    return render_template("dashboard.html")
+
+@app.route('/transactions')
+def transactions():
+    #return "Hello World!"
+    return render_template("dashboard.html")
+
+"""
+
+if __name__ == '__main__':
+    app.run(debug=True)
