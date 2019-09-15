6f6d9682c6bccd35822d09907fe4919de5bf8614
ngmhprogramming <ngmhcartoon@gmail.com>
2019-09-16 01:24:07 +0800

    signup and error notifs

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, session, redirect, url_for, request
+from base import Base, engine, Session
 from classes import User, Item, Request
 from hash import hash
 
 app = Flask(__name__)
 app.secret_key = "fixed"
+
+Base.metadata.create_all(engine)
+db_session = Session()
 
 def get_name():
     if "name" in session: return session["name"]
@@ -36,11 +40,23 @@ def signup():
     if request.method == "GET":
         return render_template("signup.html")
     else:
-        type_no = request.form["type_no"]
+        type_no = int(request.form["type_no"])
         location = request.form["location"]
         name = request.form["name"]
         password = request.form["password"]
+        if not (1 <= type_no and type_no <= 3):
+            return render_template("signup.html", error="Invalid Type")
+        if location is "":
+            return render_template("signup.html", error="No Location Specified")
+        if name is "":
+            return render_template("signup.html", error="No Name Specified")
+        if password is "":
+            return render_template("signup.html", error="No Password Specified")
         password = hash(password)
+        user = User(type_no, location, name, password)
+        db_session.add(user)
+        db_session.commit()
+        return redirect(url_for("login"))
 
 
 @app.route("/inventory")
