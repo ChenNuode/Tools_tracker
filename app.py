1daf908ea79372cdf2375166b9d58886b8af5196
ngmhprogramming <ngmhcartoon@gmail.com>
2019-09-16 00:46:20 +0800

    basic login logout working

--- app.py
+++ app.py
@@ -1,31 +1,53 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, request
 
 app = Flask(__name__)
 app.secret_key = "fixed"
 
-@app.route('/')
-def dashboard():
-    return render_template("dashboard.html")
+def get_username():
+    if "username" in session: return session["username"]
+    return False
 
-@app.route('/inventory')
+@app.route("/")
+def dashboard():
+    username = get_username()
+    if username:
+        return render_template("dashboard.html", username=username)
+    return redirect(url_for("login"))
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        username = request.form["username"]
+        password = request.form["password"]
+        session["username"] = username
+        return redirect(url_for("dashboard")) 
+
+@app.route("/logout")
+def logout():
+    session.pop("username", None)
+    return redirect(url_for("dashboard"))
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+@app.route("/inventory")
 def inventory():
-    #return "Hello World!"
     return render_template("inventory.html")
 
-@app.route('/staff')
+@app.route("/staff")
 def staff():
-    #return "Hello World!"
     return render_template("dashboard.html")
 
-@app.route('/requests')
+@app.route("/requests")
 def requests():
-    #return "Hello World!"
     return render_template("dashboard.html")
 
-@app.route('/profile')
+@app.route("/profile")
 def profile():
-    #return "Hello World!"
     return render_template("dashboard.html")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
